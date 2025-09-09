07ae6fc1858abe0e1a1cfb31f74927c734e0b921
MatasNacevicius <144778232+MatasNacevicius@users.noreply.github.com>
2025-09-09 13:04:04 +0300

    Add files via upload

---
+++ 1uzduotis.py
@@ -0,0 +1,32 @@
+# 1 uzduotis
+
+# sak=input('Iveskite sakini: ')
+# print('pagrindinis sak: ', sak )
+# print('pakeistas didziosiomis: ', sak.upper())
+# print('simboliu skaicius tekste: ', len(sak))
+
+
+# 2 uzduotis
+
+# sak=input('ivesk varda ir pavarde: ')
+# sak=sak.split(" ")
+# print(sak[0])
+
+# 3 uzduotis
+
+# sak=input('iveskite sakini: ')
+# print(sak)
+# print('sakyni sudaro', len(sak.split()), 'zodziai')
+
+# 4 uzduotis
+
+# sak=input('iveskite sakini: ')
+# print('sakinyje yra ', sak.count(' '), 'tarpai/u')
+
+# 5 uzduotis
+
+sak=input('iveskite sakini: ')
+zodis=input('iveskite zodi kuri norite keist: ')
+zodis1=input('iveskite zodi i kuri keisite: ')
+print(sak)
+print(sak.replace(zodis, zodis1))
