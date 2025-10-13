91273a344ecb3a57a7309313e4e051bd3cac4540
MatasNacevicius <144778232+MatasNacevicius@users.noreply.github.com>
2025-10-13 22:28:34 +0300

    Add files via upload

---
+++ Pasiruosimas5_19a5f0de4e2b6762afc4be8a1a4944c4.py
@@ -0,0 +1,12 @@
+﻿import numpy as np
+
+matrica=np.random.randint(low=-5, high=5, size=(5,5))
+print('Matrica:\n',matrica)
+
+
+ist=[]
+for i in range(len(matrica[0,:])):
+    ist.append(matrica[i, i])
+
+print('Istrizaine: ',np.array(ist))
+print('Istrizaine is kitos puses: ',np.array(ist[::-1]))
